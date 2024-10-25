036e3d6550919b3002be3767168c6e1e4ca10732
Brenno <151473774+brenno1206@users.noreply.github.com>
2024-10-24 22:13:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,38 +10,31 @@ def lerExcel(caminhoArquivo):
         print(f"Erro de execução: {e}")
 
 
-def verificar(funcionario): # O argumento é um registro de funcionário
+def verificar(funcionario, indice): # O argumento é um registro de funcionário
     try:
         try:
-            int(funcionario.iloc[0]) # .iloc[x] pega o registro do funcionário da coluna x, nesse caso  a matrícula
+            int(funcionario['Matrícula']) # valor da coluna 'Matrícula' da linha de indice = funcionario
         except ValueError:
-            print(f"Erro na matrícula do funcionário: {funcionario.iloc[0]}")
+            print(f"Erro na matrícula do funcionário de index {indice} -> {funcionario['Matrícula']}")
             return False
-
-        if not (str(funcionario.iloc[1]).replace(' ', '').isalpha()): # replace tira todos os espaços
-            # presentes da string
-            print(f"Erro no nome do funcionário: {funcionario.iloc[1]}")
+        if not (str(funcionario['Nome']).replace(' ', '').isalpha()): # replace tira todos os espaços presentes da string
+            print(f"Erro no nome do funcionário de index {indice} -> {funcionario['Nome']}")
             return False
-
-        if not (str(funcionario.iloc[2]).upper() in ['M','F']):
-            print(f"Erro no sexo do funcionário: {funcionario.iloc[2]}")
+        if not (str(funcionario['Sexo']).upper() in ['M','F']):
+            print(f"Erro no sexo do funcionário de index {indice} -> {funcionario['Sexo']}")
             return False
-
         try:
-            float(funcionario.iloc[3])
+            float(funcionario['Salário Hora'])
         except ValueError:
-            print(f"Erro no salário do funcionárioc: {funcionario.iloc[3]}")
+            print(f"Erro no salário do funcionárioc de index {indice} -> {funcionario['Salário Hora']}")
             return False
-
         try:
-            float(funcionario.iloc[4])
+            float(funcionario['Horas Trabalhadas'])
         except ValueError:
-            print(f"Erro nas horas do funcionário: {funcionario.iloc[4]}")
+            print(f"Erro nas horas do funcionário de index {indice} -> {funcionario['Horas Trabalhadas']}")
             return False
-
         else:
             return True
-
     except Exception as e:
         print(f"Erro de execuçao {e}")
 
@@ -60,17 +53,14 @@ def visualizar(dadosFunc, resultados, funcionariosValidos):
     try:
         visualizacao = int(input("Se quiser receber os dados consistentes de cada funcionário"
                              " digite 1, Se quiser receber apenas o total dos salários brutos digite 2: "))
-
-        soma = sum([salario[0] for salario in resultados]) # salario in resultado é o vetor de cada salário dos
-        # funcionários, tendo uma iteração aí. O salario[0] é o bruto. Então a linha está somando o salário bruto
-        # de cada funcionário e armazenando em soma
+        soma = sum([salario[0] for salario in resultados]) #salario in resultado é o vetor de cada salário dos funcionários, tendo uma iteração aí. O salario[0] é o bruto. Então a linha está somando o salário bruto de cada funcionário e armazenando em soma
 
         match visualizacao:
             case 1:
                 contador = 0 # Para selecionar os funcionários armazenados dentro do vetor Resultado
                 for i in funcionariosValidos: # i representa o código dos funcionários válidos
-                    print(f"Matrícula: {dadosFunc.loc[i].iloc[0]} - " #seleciona a linha i da coluna 0
-                        f"Nome: {dadosFunc.loc[i].iloc[1]} - " 
+                    print(f"Matrícula: {dadosFunc.loc[i]['Matrícula']} - " #seleciona a linha i da coluna 0
+                        f"Nome: {dadosFunc.loc[i]['Nome']} - " 
                         f"Salário Bruto: {resultados[contador][0]:.2f} - " #seleciona a linha contador da coluna 0
                         f"Valor dos descontos: {resultados[contador][1]:.2f} - "
                         f"Salário Líquido: {resultados[contador][2]:.2f} ")
@@ -83,25 +73,25 @@ def visualizar(dadosFunc, resultados, funcionariosValidos):
     except ValueError:
         print("Entrada não válida")
     except Exception as e:
-        print(f"Erro de execução {e}")
+        print(f"Erro de execução: {e}")
 
+# INÍCIO DO CÓDIGO
+try:
+    arquivo = r'/Users/brennog/Desktop/Pasta1.xlsx'
+    dataFrame = lerExcel(arquivo)
 
+    num_indices = dataFrame.shape[0] # .Shape[x] retorna a quantidade de linhas da coluna x
+    funcionariosValidosIndices = []
 
-arquivo = r'/Users/brennog/Desktop/Pasta1.xlsx'
-dataFrame = lerExcel(arquivo)
+    for indice in range(num_indices): # Para cada funcionário de 0 ao final
+        linha = dataFrame.loc[indice] # Seleciona todos os registros de uma linha, ou seja do funcionário de indice .loc[x]
+        if verificar(linha, indice): # Se a verificação for verdadeira, o número de registro do usuário é guardado no vetor de funcionariosValidos
+            funcionariosValidosIndices.append(indice)
 
-numFunc = dataFrame.shape[0] # .Shape[x] retorna a quantidade de linhas da coluna x
-funcionariosValidos = []
+    funcionariosSalarios = []
+    for indiceValido in funcionariosValidosIndices:
+        funcionariosSalarios.append(calcularSalario(indiceValido,dataFrame.loc[indiceValido])) # a função retorna um vetor de vetores
 
-for func in range(numFunc): # Para cada funcionário de 0 a numFunc
-    linha = dataFrame.loc[func] # Seleciona todos os registros de uma linha, ou seja do funcionário
-    if verificar(linha): # Se a verificação for verdadeira, o número de registro do usuário é guardado no vetor de
-        # funcionariosValidos
-        funcionariosValidos.append(func)
-
-funcionariosSalarios = []
-for calculo in funcionariosValidos:
-    funcionariosSalarios.append(calcularSalario(calculo,dataFrame.loc[calculo])) # a função retorna um vetor de vetores
-
-visualizar(dataFrame, funcionariosSalarios, funcionariosValidos) # Para visualizar, precisa do Dataframe, dos
-# funcionários válidos e do salário de cada um
+    visualizar(dataFrame, funcionariosSalarios, funcionariosValidosIndices) # Para visualizar, precisa do Dataframe, dos funcionários válidos e do salário de cada um
+except Exception as e:
+    print(f"Erro de execução: {e}")
